9b00f377e8f8eb16ba26e299c1dbc470a6467a93
XXWIERTARAXX <165953508+XXWIERTARAXX@users.noreply.github.com>
2026-01-13 16:10:02 +0100

    Update games array and change user status

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var games = [730];  // Enter here AppIDs of the needed games
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
+user.logOn({"accountName": l0wz00m, "password": sHdUa82f1f@z, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
 user.on('loggedOn', () => {
 	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
 	user.setPersona(status);               
